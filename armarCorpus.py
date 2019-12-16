e55777e3b6b4cb07259bd9d7345fa263f2d1fe4a
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2019-12-15 21:21:54 -0300

    actualizada p1 a, b con nuevo corpus

--- armarCorpus.py
+++ armarCorpus.py
@@ -5,9 +5,7 @@ libros = ['frankenstein.txt', 'moby_dick.txt', 'modest_proposal.txt',
           'jekyll_hyde.txt', 'two_cities.txt', 'pride_prejudice.txt', 
           'beowulf.txt', 'dracula.txt', 'metamorphosis.txt', 'ulysses.txt']
 
-#libros = ['frankenstein.txt', 'moby_dick.txt', 'modest_proposal.txt', 
- #           'two_cities.txt', 'metamorphosis.txt']
-
+# Armado corpus
 output = open('./corpus.txt', 'w')
 for libro in libros:
     archivo = open('./libros/'+libro, 'r')
@@ -22,20 +20,3 @@ for libro in libros:
         output.write(corpus)
     archivo.close()
 output.close()
-
-'''
-# Realizar conteo
-cuenta_palabras = dict()
-nLineas = 0
-for linea in corpus.split('\n'):
-    nLineas += 1
-    for palabra in linea.split():
-        # Separar palabras de símbolos
-        if not palabra.isalnum():
-            continue
-        # Estandarizar a minúsculas
-        palabra = palabra.lower()
-        if palabra in cuenta_palabras:
-            cuenta_palabras[palabra] += 1
-        else:
-            cuenta_palabras[palabra] = 1'''
